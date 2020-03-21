36395f724e4675eb7c787c783283805aba1be4b4
Lukasz Lawicki <lla@trackman.com>
2020-03-21 21:29:21 +0100

    WIP 👑

---
+++ Program.fs
@@ -0,0 +1,25 @@
+﻿open System.IO
+open System
+
+let checkIfContainsTodo (line : string, fileInfo : FileInfo) =
+    if (line.ToLower().Contains("todo")) then printfn "In %s: %s" fileInfo.Name line
+
+let printFileIntfo (fileInfo : FileInfo) =
+    use file = File.OpenText(fileInfo.FullName)
+    let mutable valid = true
+    while (valid) do
+        let line = file.ReadLine()
+        if (line = null) then valid <- false
+        else checkIfContainsTodo (line, fileInfo)
+
+
+let main() =
+    let footballPracticePath = @"/Users/lukaszlawicki/Documents/Coding/FootballPractice"
+    for folder in Directory.GetDirectories(footballPracticePath, "*", SearchOption.AllDirectories) do
+
+        let currentDirectory = new DirectoryInfo(folder)
+
+        for csharpFile in currentDirectory.GetFiles("*.cs") do
+            printFileIntfo (csharpFile)
+
+main()
